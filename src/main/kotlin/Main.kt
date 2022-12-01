ee55d63fcf440909acf574a351a558e58c235f81
Neonader <quintenpotma@outlook.com>
2022-12-01 15:09:56 +0100

    Day 1a

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 fun main(args: Array<String>) {
-    print("Which day would you like to run? > ")
-    when (readln()) {
-        "1a" -> day01.a()
-        "1b" -> day01.b()
-    }
+  print("Which day would you like to run? > ")
+
+  val input = readln()
+  when (input) {
+    "1a" -> day01.a()
+    "1b" -> day01.b()
+  }
 }
--- src/main/kotlin/day01/DayOne.kt
+++ src/main/kotlin/day01/DayOne.kt
@@ -1,10 +1,27 @@
 package day01
 
+import java.io.File
+
 fun a() {
-    println("running 1a...")
-}
-fun b() {
-    println("running 1b...")
+  val fileList = File("puzzle_input\\day01.txt").useLines { it.toList() }
+
+  var mostCalories = 0
+  var calories = 0
+
+  for (line in fileList) {
+    if (line == "") {
+      calories = 0
+    } else {
+      calories += line.toInt()
+    }
+
+    if (calories > mostCalories) mostCalories = calories
+  }
+
+  println(mostCalories)
 }
 
+fun b() {
+  println("running 1b...")
+}
 
