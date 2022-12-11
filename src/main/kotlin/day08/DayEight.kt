9ec3fad0cc24ede49c3d62309bdbbc2a3a4dcdfa
Neonader <quintenpotma@outlook.com>
2022-12-11 21:53:47 +0100

    Day 8b

--- src/main/kotlin/day08/DayEight.kt
+++ src/main/kotlin/day08/DayEight.kt
@@ -4,13 +4,15 @@ import java.io.File
 
 val fileList =  File("puzzle_input/day08.txt").useLines { it.toList() }
 val forest = fileList.map { row -> row.toList().map { col -> col.code } }
+val width = forest[0].size
+val height = forest.size
 
 fun visible(row: Int, col: Int): Boolean {
   val tree = forest[row][col]
 
   if (
-    row == 0 || row == 98 ||
-    col == 0 || col == 98
+    row == 0 || row == height ||
+    col == 0 || col == width
   ) return true
 
   var visible = true
@@ -28,20 +30,59 @@ fun visible(row: Int, col: Int): Boolean {
 
 // visible from right
   visible = true
-  for (otherTree in forest[row].slice(col + 1 until forest[row].size))
+  for (otherTree in forest[row].slice(col + 1 until width))
     visible = visible && otherTree < tree
   if (visible) return true
 
 // visible from below
   visible = true
-  for (otherTree in forest.slice(row + 1 until forest[col].size))
+  for (otherTree in forest.slice(row + 1 until height))
     visible = visible && otherTree[col] < tree
   return visible
 }
 
 fun scenicScore(row: Int, col: Int): Int {
   val tree = forest[row][col]
-  var score = -1
+  var score = 1
+
+  if (
+    row == 0 ||
+    row == height ||
+    col == 0 ||
+    col == width
+  ) return 0
+
+// up distance
+  var distance = 0
+  for (otherTree in forest.slice(row - 1 downTo 0)) {
+    distance++
+    if (otherTree[col] >= tree) break
+  }
+  score *= distance
+
+// left distance
+  distance = 0
+  for (otherTree in forest[row].slice(col - 1 downTo 0)) {
+    distance++
+    if (otherTree >= tree) break
+  }
+  score *= distance
+
+// right distance
+  distance = 0
+  for (otherTree in forest[row].slice(col + 1 until width)) {
+    distance++
+    if (otherTree >= tree) break
+  }
+  score *= distance
+
+// down distance
+  distance = 0
+  for (otherTree in forest.slice(row + 1 until height)) {
+    distance++
+    if (otherTree[col] >= tree) break
+  }
+  score *= distance
 
   return score
 }
@@ -49,7 +90,7 @@ fun scenicScore(row: Int, col: Int): Int {
 fun a(): Int {
   var sum = 0
 
-  for (row in 0..98) for (col in 0..98)
+  for (row in 0 until height) for (col in 0 until width)
     if (visible(row, col)) sum++
 
   return sum
@@ -58,7 +99,7 @@ fun a(): Int {
 fun b(): Int {
   var max = 0
 
-  for (row in 0..98) for (col in 0..98)
+  for (row in 0 until height) for (col in 0 until width)
     if (scenicScore(row, col) > max)
       max = scenicScore(row, col)
 
